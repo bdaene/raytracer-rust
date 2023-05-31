c09c1a0fa6bc31767ec385f07b1c8b86bf179bef
Benoît Daene <benoitdaene@gmail.com>
2023-06-01 00:01:56 +0200

    Add power to lights.

--- src/scene/object/materials/light.rs
+++ src/scene/object/materials/light.rs
@@ -1,6 +1,6 @@
 use super::Material;
-use crate::scene::object::colors::Color;
-use crate::{utils::hit::ShapeHit, scene::object::colors::Colors};
+use crate::scene::object::colors::{Color, Colors};
+use crate::utils::hit::ShapeHit;
 use crate::utils::ray::Ray;
 use palette::LinSrgb;
 use serde::{Deserialize, Serialize};
@@ -8,10 +8,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Light {
     pub color: Colors,
+    pub power: f32,
 }
 
 impl Material for Light {
     fn bounce_ray(&self, ray: Ray, t: f64, shape_hit: &ShapeHit) -> (LinSrgb, Option<Ray>) {
-        (self.color.get_color(ray, t, shape_hit), None)
+        let color = self.color.get_color(ray, t, shape_hit) * self.power;
+        (color, None)
     }
 }
